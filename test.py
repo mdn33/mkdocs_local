e5cca8c930911ac7778fa5a8a502172e965c6368
Oracle Public Cloud User <opc@internal-repository.sub11012222420.vcnbds.oraclevcn.com>
2023-10-14 15:37:19 +0000

    f

--- test.py
+++ test.py
@@ -1,5 +1,36 @@
+def bar(stringa):
 
+    """Hello, world!
+    
+    - Input: None
+    - Output: None.
+    """
+    
+    print(stringa)
+    
+    
+def bar2(stringa):
 
+    """Hello, world!
+    
+    - Input: None
+    - Output: None.
+    """
+    
+    print(stringa)
+    
 
-def stampa():
-    print('hell');
+class MyClass:
+    """Print print print!"""
+
+    def method_a(self):
+        """Print A!"""
+        print("A!")
+
+    def method_b(self):
+        """Print B!"""
+        print("B!")
+
+    def method_c(self):
+        """Print C!"""
+        print("C!")
