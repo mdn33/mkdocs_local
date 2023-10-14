cf28ebc86f73a15d7fc069b12bdf313213c9f3ba
Oracle Public Cloud User <opc@internal-repository.sub11012222420.vcnbds.oraclevcn.com>
2023-10-14 16:20:06 +0000

    a

--- test.py
+++ test.py
@@ -24,8 +24,9 @@ class MyClass:
     """Print print print!"""
 
     def method_a(self):
-        """Print A!"""
-        print("A!")ssssss
+
+        """Print A!"""sssssssss
+        print("A!")
 
     def method_b(self):
         """Print B!"""
